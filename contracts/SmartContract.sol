2365b2563e3a8c96ab7c2ea1191e7ab0dacd0020
AnthonyRS69 <anthony.richer@isitech.fr>
2026-04-09 13:11:13 +0200

    Gestion Vote Smart Contract

--- contracts/SmartContract.sol
+++ contracts/SmartContract.sol
@@ -45,3 +45,89 @@ contract Vote {
         emit Transfer(address(0), to, tokenId);
     }
 //==================FIN PARTIE JETON====================================
+
+//==================PARTIE VOTE====================================
+
+    //================Initialisation des variables====================
+
+    struct Candidate {string  name; uint256 nb_vote;} // on creer une liste candidat dans lequel on va prendre un nom et un nb de vote
+
+    address public owner;
+    bool public voteOuvert;
+
+    Candidate[] public candidates; // on creer une liste candidat
+
+    mapping(address => bool) public isVotant;           // on regarde si l'adresse est un votant ou non
+    mapping(address => bool) public aVoter;             // on regarde si l'adresse à deja voter ou non
+    mapping(address => uint256) public voterToken;      // adresse lier au tokenId recu au vote (si =0 alors pas voté)
+    mapping(uint256 => uint256) public voterCandidat;   // lie le jeton token au candidat
+
+    //==================Lecture des candidats===============
+    function getNbCandidat() external view returns (uint256) {
+        return candidates.length;
+    }
+
+    function getCandidat(uint256 idCandidat) external view returns (string candidateName, uint256 nb_vote)
+    {
+        require(idCandidat < candidates.length, "Candidat inexistant dans les candidats participant");
+        Candidate c = candidates[idCandidat];
+        return (c.name, c.nb_vote);
+    }
+
+    //===========Gestion du process de vote=================
+
+    event VotantAjouter(address indexed voter); // Evenement pour dire qu'on a ajouter un votant
+    event VoteCommencer();                      // Evenement pour dire que les votes ont commencé
+
+    modifier onlyOwner() {  // permet de rajouter une condirion a une fonctionpour que suel l'admin puisse y acceder
+        require(msg.sender == owner, "Pas l'Owner");
+        _;
+    }
+
+    modifier onlyVoter() {  // pareil pour les votants
+        require(isVotant[msg.sender], "Ne peut pas voter");
+        _;
+    }
+
+    modifier votingIsOpen() {   // pareil quand les votes sont ouverts
+        require(voteOuvert, "Les votes ne sont pas ouvert");
+        _;
+    }
+
+    constructor(string[] candidateNames) {  // on prend une liste de candidat en parametre qui seront ceux pour qui on peut voter
+        require(candidateNames.length >= 2, "Need at least 2 candidates");
+
+        owner = msg.sender;
+        idToken = 1; // les token_Id commencent à 1
+
+        for (uint256 i = 0; i < candidateNames.length; i++) {
+            candidates.push(Candidate({ name: candidateNames[i], nb_vote: 0 })); // on les mets dans la liste des candidat avec le nb de vote recu
+        }
+    }
+
+    function addVoter(address votant) external onlyOwner { // on permet a l'admin uniquement d'ajouter des votant
+        require(votant != address(0), "Aucune adresse renseignée");
+        require(!isVotant[votant], "Adresse est déjà un votant");
+        isVotant[votant] = true;
+        emit VotantAjouter(votant);
+    }
+
+    function startVote() external onlyOwner { // Permet d'ouvrir les vote
+        require(!voteOuvert, "Les votes sont déjà ouvert");
+        voteOuvert = true;
+        emit VoteCommencer();
+    }
+
+    function vote(uint256 candidateId) external onlyVoter votingIsOpen { // permet au votant de voter
+        require(!aVoter[msg.sender], "A deja vote");
+        require(candidateId < candidates.length, "Candidat inexistant");
+        aVoter[msg.sender]= true;               // il a voter
+        candidates[candidateId].nb_vote += 1;  // +1 vote pour celui qui vote
+        uint256 tokenId = idToken;
+        idToken += 1;                           // Jeton +1
+        voterToken[msg.sender]= tokenId;  
+        voterCandidat[tokenId]= candidateId;    // on inscris qui a vote avec ce token
+
+        _mint(msg.sender, tokenId);
+    }
+}
